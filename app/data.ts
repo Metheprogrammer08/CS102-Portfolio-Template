6c1b3a46662d319701d125e8c3faf485b5acd9e7
Metheprogrammer08 <95514324+Metheprogrammer08@users.noreply.github.com>
2025-05-18 16:12:27 +0700

    Update data.ts
    Add personal info

--- app/data.ts
+++ app/data.ts
@@ -27,16 +27,16 @@ type SocialLink = {
   link: string
 }
 
-export const EMAIL = ''
+export const EMAIL = 'daviddoforwork@gmail.com'
 
-export const WEB_TITLE = 'Name - Personal Website'
+export const WEB_TITLE = 'Đỗ Như Hải\' - Portfolio'
 export const WEB_DESCRIPTION =
   'A personal website showcasing my work and projects.'
-export const FOOTER_COPYRIGHT = '© 2025 Nguyen Van B.'
-export const FOOTER_LINK = 'https://github.com/username/'
+export const FOOTER_COPYRIGHT = '© 2025 Đỗ Như Hải.'
+export const FOOTER_LINK = 'https://github.com/metheprogrammer08/'
 
-export const NAME = 'Nguyen Van B'
-export const JOB_TITLE = 'NASA Frontend Intern'
+export const NAME = 'Đỗ Như Hải'
+export const JOB_TITLE = 'High School student'
 export const DESCRIPTION =
   'I am a passionate designer and developer with a focus on creating beautiful and functional user interfaces. I have experience in design engineering, front-end development, and product design. I love to explore the intersection of design and technology, and I am always looking for new challenges to tackle.'
 
